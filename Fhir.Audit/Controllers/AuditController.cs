b6b5563b64fb8983e75c0999575712474ca38387
Larry Richardson <lrichardson@hchb.com>
2018-05-03 13:05:25 -0400

    Initial

---
+++ Fhir.Audit/Controllers/AuditController.cs
@@ -0,0 +1,30 @@
+﻿namespace Fhir.Audit.Controllers
+{
+
+    using System.Threading.Tasks;
+
+    using Hl7.Fhir.Model;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using Task = System.Threading.Tasks.Task;
+
+    [Produces("application/json")]
+    [Route("api/Audit")]
+    public class AuditController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> AuditEvent([FromBody] AuditEvent auditEvent)
+        {
+            if (auditEvent == null)
+            {
+                throw new System.ArgumentNullException(nameof(auditEvent));
+            }
+
+            bool retval = await Task.FromResult(true);
+
+            return this.Ok(retval);
+        }
+
+    }
+}
